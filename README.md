e3775735044fb030285143d3fc469ac54fe706af
AlexLancrajan <alexlancrajan@gmail.com>
2024-08-15 17:49:06 +0200

    updated app to include database functionality and minor corrections

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # phonebook-backend
+
+This is my progress in the open fullstack developer course by the University of Helsinki. I will update and add things throughout my journey, so stay tuned.
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Contact = require('./models/contact')
 
 app.use(express.static('dist'))
 
@@ -13,80 +15,98 @@ morgan.token('body', (req) => {
 })
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
-
-let persons = [
-  { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (req, res, next) => {
+  Contact.find({})
+    .then(contacts => {
+    res.json(contacts)
+    })
+    .catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(person => person.id === id)
-    
-  if(person){
-    res.json(person)
-  } 
-  else {
-    res.status(404).end()
+app.get('/api/persons/:id', (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then(contact => {
+      if (contact){
+        res.json(contact)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(err => next(err))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  if (body.name === "" || body.number === "") {
+    return res.status(400).json({error: 'content missing'})
   }
 
+  const contact = new Contact({
+    name: body.name,
+    number: body.number
+  })
+
+  contact.save()
+  .then(savedContact => {
+    res.json(savedContact)
+  })
+  .catch(err => next(err))
 })
 
-app.post('/api/persons', (req, res) => {
-  const person = req.body
-  const name = person.name
+app.put('/api/persons/:id',(req, res, next) => {
+  const body = req.body
 
-  if(!person.name || !person.number) {
-    return res.status(400).json({error: 'name or number missing'})
+  const contact = {
+    name: body.name,
+    number: body.number,
   }
-  if(persons.find(person => person.name === name)) {
-    return res.status(400).json({error: 'name already in phonebook'})
-  }
 
-  person.id = String(Math.floor(Math.random()*100000))
-  persons = persons.concat(person)
-  res.json(person)
+  Contact.findByIdAndUpdate(req.params.id, contact, {new: true})
+    .then(updatedContact => {
+      res.json(updatedContact)
+    })
+    .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Contact.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
 
-app.get('/info', (req, res) => {
-    const numPersons = persons.length
-    const timeOfReq = new Date().toUTCString()
-
-    const response = `<p>Phonebook has info for ${numPersons} people</p><p>${timeOfReq}</p>`
+app.get('/info', (req, res, next) => {
+  const timeOfReq = new Date().toUTCString()
+  
+  Contact.countDocuments({})
+  .then(count => {
+    const response = `<p>Phonebook has info for ${count} people</p><p>${timeOfReq}</p>`
     res.send(response)
+  })
+  .catch(err => next(err))
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({error: 'maformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
